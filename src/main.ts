aee49b388d48f6b06208554191e3952463701891
penso996 <penso.andrea@outlook.it>
2025-07-08 14:20:55 +0200

    snack 1

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log("Hello!");
+// Snack 1
+let dato: unknown;
+
+if (typeof dato === "string") {
+  console.log(dato.toUpperCase());
+} else if (typeof dato === "number") {
+  let total = dato * 2;
+  console.log(total);
+} else if (typeof dato === "boolean") {
+  console.log(dato ? "Sì" : "No")
+} else {
+  console.log("Tipo non supportato");
+}
