f0b588b4414769fc5dec38893d277c9449fd737e
penso996 <penso.andrea@outlook.it>
2025-07-08 14:24:18 +0200

    snack 1 bonus

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,13 @@ if (typeof dato === "string") {
   let total = dato * 2;
   console.log(total);
 } else if (typeof dato === "boolean") {
-  console.log(dato ? "Sì" : "No")
+  console.log(dato ? "Sì" : "No");
+} else if (dato === null) {
+  console.log("Il dato è vuoto");
+} else if (Array.isArray(dato)) {
+  console.log(dato.length);
+} else if (dato instanceof Promise) {
+  dato.then((msg) => console.log(msg));
 } else {
   console.log("Tipo non supportato");
 }
